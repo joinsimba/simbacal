a3ede23cc69a3ff66b872db4ffb8ba830e163c1b
Simba Mentor <joinsimba@gmail.com>
2021-07-29 22:23:41 +0000

    add react

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+class HelloWorld extends React.Component {
+  render () {
+    return <>Greeting: {this.props.greeting}</>
+  }
+}
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string
+}
+export default HelloWorld
